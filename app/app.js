14d33bf210e5efa162a354747a7b8f3e7e66c5ba
TShelton <todd@devworkshop.org>
2020-10-07 12:35:20 -0400

    Initial commit

---
+++ app/app.js
@@ -0,0 +1,9 @@
+/**
+ * This function will get the exact data you need from firebase.
+ * @author Todd Shelton
+ * @param {string} id - You need to pass over the exact comments id to get the right object
+ * @param {object} comment - this is the entire comment for the post
+ * @return {void}
+ */
+
+function getExactData(id, comment) {}
